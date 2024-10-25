b503095aeee7314b2780a62ba9d0397f0a74156c
Dario <darioken@gmail.com>
2024-10-25 16:06:39 -0300

    Cambios

--- src/app/modules/admin-portal/validation-to-program/list/list.component.ts
+++ src/app/modules/admin-portal/validation-to-program/list/list.component.ts
@@ -25,6 +25,7 @@ import { GenericHeaderComponent } from 'app/shared/components/generic-header/gen
 import { GenericTableComponent } from 'app/shared/components/generic-table/generic-table.component';
 import { GenericTableHandler } from 'app/shared/components/generic-table/generic-table.interface'
 // Datos Dummy
+import { columnsData } from './columns-data'; // Importar el nuevo archivo
 
 @Component({
   selector: 'app-admin-validation-to-program-list',
@@ -74,35 +75,7 @@ export class ValidationToProgramListComponent implements OnInit, OnDestroy, Gene
   formRoot: UntypedFormGroup;
   clearVisible: boolean = false;
 
-  data = [
-    {
-      nombre: 'FONDITA DE JUAN',
-      uie: '7454839948',
-      registroCorp: '789013',
-      ssPatronal: '66-145674',
-      creadoPor: 'Juan Del Pueblo',
-      estatus: 'Pendiente a validar',
-      fechaCreacion: 'Agosto 3, 2024',
-    },
-    {
-      nombre: 'YMCA',
-      uie: '873654298',
-      registroCorp: '145298',
-      ssPatronal: '77-579012',
-      creadoPor: 'Sandra Díaz',
-      estatus: 'Orientación',
-      fechaCreacion: 'Septiembre 2, 2024',
-    },
-    {
-      nombre: 'EBENEZER',
-      uie: '983452672',
-      registroCorp: '256378',
-      ssPatronal: '33-357489',
-      creadoPor: 'Omar Colón',
-      estatus: 'Visita Pre-operacional',
-      fechaCreacion: 'Octubre 2, 2024',
-    },
-  ];
+  data = columnsData; // Asignar los datos desde el nuevo archivo
 
   // Constructor
   constructor() {}
