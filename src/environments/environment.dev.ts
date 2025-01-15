df38290a6a13f4587bfb20094d358666660530d9
Dario <darioken@gmail.com>
2025-01-15 17:11:23 -0300

    15-01-2025
    Cambios para Git e implementaciones de ambientes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ import data from './urls.json'; // Es diferente en la version 13
 
 export const environment = {
   production: false,
-  baseHttpUrl: data.localUrl,
+  baseHttpUrl: data.devUrl,
   v100: 1.0,
   loginEnable: false,
 };
