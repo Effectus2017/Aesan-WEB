f02144fd608af8e370b8b9739be260d931fc0872
Dario Neira <darioken@gmail.com>
2026-03-03 14:57:22 -0300

    03-03-2026
    Refactor: Update styles in generic header and route style service
    
    Changes include:
    
    - Updated button styles in the generic header component for improved visual consistency.
    - Simplified the updateRouteStyles method in the route style service by removing conditional logic and directly assigning background class and logo path, enhancing maintainability.

--- src/app/shared/services/route-style.service.ts
+++ src/app/shared/services/route-style.service.ts
@@ -13,39 +13,8 @@ export class RouteStyleService {
      * @returns Un objeto con la clase de fondo y la ruta del logo
      */
     updateRouteStyles(): { backgroundClass: string; logoPath: string } {
-        const userPrograms = this._authService.getUserPrograms();
-
-        let backgroundClass = 'bg-[#003C49]';
-        let logoPath = 'assets/images/logo/aesan.png';
-
-        if (userPrograms) {
-            switch (userPrograms) {
-                case PROGRAM_CODES.PDAM:
-                    backgroundClass = 'bg-[#4C3152]';
-                    logoPath = 'assets/images/logo/pdam-150x150.png';
-                    break;
-                case PROGRAM_CODES.PSAV:
-                    backgroundClass = 'bg-[#F26B1E]';
-                    logoPath = 'assets/images/logo/psav-150x150.png';
-                    break;
-                case PROGRAM_CODES.PACNA:
-                    backgroundClass = 'bg-[#F8B100]';
-                    logoPath = 'assets/images/logo/pacna-150x150.png';
-                    break;
-                case PROGRAM_CODES.PFHF:
-                    backgroundClass = 'bg-[#28AF66]';
-                    logoPath = 'assets/images/logo/pfhf-150x150.png';
-                    break;
-                case PROGRAM_CODES.PAF:
-                    backgroundClass = 'bg-[#4C3152]';
-                    logoPath = 'assets/images/logo/pdam-150x150.png';
-                    break;
-                case PROGRAM_CODES.PDFE:
-                    backgroundClass = 'bg-[#2A788A]';
-                    logoPath = 'assets/images/logo/pdfe-150x150.png';
-                    break;
-            }
-        }
+        const backgroundClass = 'bg-[#003C49]';
+        const logoPath = 'assets/images/logo/aesan.png';
 
         return { backgroundClass, logoPath };
     }
